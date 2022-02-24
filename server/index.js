5b1d762f983c4653b38b9f2be71a195ea24600cc
eru123 <yeoligoakino@gmail.com>
2022-02-24 07:02:24 +0000

    update

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const Fastify = require('fastify');
+const { PrismaClient } = require('@prisma/client');
+
+async function start(){
+    const app = Fastify({ logger: true });
+    // const prisma = new PrismaClient({});
+
+    app.decorate('prisma', prisma);
+    await app.register(require('fastify-cors'), {})
+
+    app.get('/log', async (req, res) => { 
+        res.send({logged: true})    
+    })
+
+    app.get('*', async (req, res) => { 
+        res.send([1,2,3,4,5])    
+    })
+
+    app.listen(3000, (err, address) => {
+        if (err) {
+			console.log('Failed to start server')
+			throw err
+		}
+        console.log('Server started\n\n', address)
+    })
+}
+
+
+start()
